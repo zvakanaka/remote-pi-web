d6e999817209dd4b4b75f28b7932536d49141372
Adam Quinton <quintonish@gmail.com>
2021-01-18 19:57:30 -0700

    add e-ink support

--- routes.js
+++ routes.js
@@ -1,6 +1,7 @@
 const router = require('express').Router()
 const renderPage = require('./lib/renderPage')
 const { read } = require('./lib/file-async')
+const getScreen = require('./lib/screen/getScreen')
 
 router.get('/', async (req, res) => {
   const pageContent = await read('./views/index.html')
@@ -8,5 +9,16 @@ router.get('/', async (req, res) => {
 
   res.status(200).send(renderPage('Remote Pi', pageContent, styleContent))
 })
+
+router.get('/e-ink', async (req, res) => {
+  const screen1 = await getScreen()
+  const screen = await read('./screen/screenshot.jpg', 'base64')
+  const pageContent = await read('./views/e-ink.html')
+  const styleContent = ''//await read('./views/main.css')
+//    const rotatedImage = await sharp('./screen/screenshot.jpg').rotate(270).toBuffer()
+//       const screen = rotatedImage.toString('base64');
+    res.status(200).send(renderPage('Remote Pi', pageContent.replace('${screen}', screen), styleContent))
+})
+
 
 module.exports = router
