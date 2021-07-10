258f1a035e26a859995c0623d548fc49403b42f4
Adam Quinton <quintonish@gmail.com>
2021-07-09 22:05:06 -0600

    native x11 screen capture (2x speed!)

--- sockets.js
+++ sockets.js
@@ -1,7 +1,29 @@
 const socket = require('socket.io')
-const getScreen = require('./lib/screen/getScreen')
+const nativeGetScreen = require('./lib/screen/screen')
+const scrot = require('./lib/screen/scrot')
 const sendMouseEvent = require('./lib/screen/sendMouseEvent')
 const sendKeyEvent = require('./lib/screen/sendKeyEvent')
+
+const captureMethod = process.env.CAPTURE_METHOD || 'native'
+const captureFunc = captureMethod === 'native' ? nativeGetScreen : scrot
+console.log(`using capture method '${captureMethod}'`)
+
+if (process.env.VIEW_ONLY) {
+  console.log('VIEW_ONLY mode')
+}
+
+let lastRenderTime = Date.now()
+const REFRESH_INTERVAL_MS = process.env.REFRESH_INTERVAL_MS || 0.5 * 1000
+
+const throttledGetScreen = async (data, socket) => {
+  if (Date.now() - lastRenderTime >= REFRESH_INTERVAL_MS) {
+    lastRenderTime = Date.now()
+    const screen = await captureFunc()
+    socket.emit('render', screen)
+  } else {
+    console.log(`ignoring screen request (too many too fast)`)
+  }
+}
 
 module.exports = function sockets(server) {
   const io = socket.listen(server)
@@ -10,20 +32,17 @@ module.exports = function sockets(server) {
     console.log(`Connected to socket, id: ${socket.id}`)
 
     socket.on('screen', async (data) => {
-      const screen = await getScreen()
-      socket.emit('render', screen)
+      await throttledGetScreen(data, socket)
     })
 
     if (!process.env.VIEW_ONLY) {
       socket.on('mouse', async (data) => {
         await sendMouseEvent(data)
-        const screen = await getScreen()
-        socket.emit('render', screen)
+        await throttledGetScreen(data, socket)
       })
       socket.on('key', async (data) => {
         await sendKeyEvent(data)
-        const screen = await getScreen()
-        socket.emit('render', screen)
+        await throttledGetScreen(data, socket)
       })
     }
   })
