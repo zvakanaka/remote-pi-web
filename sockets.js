5cddb311723523acd628f2d94614e1964a71e214
Adam <8365885+zvakanaka@users.noreply.github.com>
2021-07-15 08:08:41 -0600

    speed up by reusing buffer and determining screen size at startup

--- sockets.js
+++ sockets.js
@@ -1,4 +1,5 @@
 const socket = require('socket.io')
+const setupNativeGetScreen = require('./lib/screen/setupNativeGetScreen')
 const nativeGetScreen = require('./lib/screen/screen')
 const scrot = require('./lib/screen/scrot')
 const sendMouseEvent = require('./lib/screen/sendMouseEvent')
@@ -13,36 +14,38 @@ if (process.env.VIEW_ONLY) {
 }
 
 let lastRenderTime = Date.now()
-const REFRESH_INTERVAL_MS = process.env.REFRESH_INTERVAL_MS || 0.5 * 1000
+const REFRESH_INTERVAL_MS = process.env.REFRESH_INTERVAL_MS || 0.1 * 1000
 
-const throttledGetScreen = async (data, socket) => {
+const throttledGetScreen = async (data, socket, options) => {
   if (Date.now() - lastRenderTime >= REFRESH_INTERVAL_MS) {
     lastRenderTime = Date.now()
-    const screen = await captureFunc()
+    const screen = await captureFunc(options)
     socket.emit('render', screen)
   } else {
     console.log(`ignoring screen request (too many too fast)`)
   }
 }
 
-module.exports = function sockets(server) {
+module.exports = async function sockets(server) {
   const io = socket.listen(server)
+
+  const initOptions = await setupNativeGetScreen()
 
   io.on('connection', function onConnection(socket) {
     console.log(`Connected to socket, id: ${socket.id}`)
 
     socket.on('screen', async (data) => {
-      await throttledGetScreen(data, socket)
+      await throttledGetScreen(data, socket, initOptions)
     })
 
     if (!process.env.VIEW_ONLY) {
       socket.on('mouse', async (data) => {
         await sendMouseEvent(data)
-        await throttledGetScreen(data, socket)
+        await throttledGetScreen(data, socket, initOptions)
       })
       socket.on('key', async (data) => {
         await sendKeyEvent(data)
-        await throttledGetScreen(data, socket)
+        await throttledGetScreen(data, socket, initOptions)
       })
     }
   })
