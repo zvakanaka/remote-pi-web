fef59694eb6156e517f967483dace756e49fad20
zvakanaka <adam.quinton@familysearch.org>
2022-01-11 23:13:18 -0700

    support other OSo

--- scripts/mac.cpp
+++ scripts/mac.cpp
@@ -24,11 +24,12 @@ void getScreen(const int xx,const int yy,const int W, const int H, const int res
   /* get pixels */
   CGDataProviderRef provider = CGImageGetDataProvider(img);
   CFDataRef dataRef = CGDataProviderCopyData(provider);
-  data = (unsigned char*)CFDataGetBytePtr(dataRef);
-
+  unsigned char* image = (unsigned char*)CFDataGetBytePtr(dataRef);
   int x, y;
   int ii = 0;
   float ratio = H / resizedHeight;
+printf("%d %d %d %d\n", xx, yy, W, H);
+printf("%d %d %d %d\n", resizedWidth, resizedHeight, ratio, ratio);
   //   printf("%f\n", ratio);
   for (y = 0; y < resizedHeight; y++) {
     for (x = 0; x < resizedWidth; x++) {
@@ -36,10 +37,14 @@ void getScreen(const int xx,const int yy,const int W, const int H, const int res
       int yR = y * ratio;
       //  printf("%d,%d \n", (int)xR, (int)yR);
       // unsigned long pixel = XGetPixel(data,(int)xR,(int)yR);
-      unsigned long pixel = ((W * (int)yR) + (int)xR) * 4;
-      unsigned char red   = data[pixel];
-      unsigned char green = data[pixel + 1];
-      unsigned char blue  = data[pixel + 2];
+      // unsigned long pixel = ((W * (int)yR) + (int)xR) * 4;
+      unsigned long pixel = (W * y * 2 + x) * 8;
+      unsigned char red   = image[pixel + 2];
+      unsigned char green = image[pixel + 1];
+      unsigned char blue  = image[pixel];
+      // if ((int)yR == 32) {
+      //   printf("%d,%d,%d,%d,%d \n", (int)xR, (int)yR, (int)red, (int)green, (int)blue);
+      // }
 
       data[ii + 2] = blue;
       data[ii + 1] = green;
